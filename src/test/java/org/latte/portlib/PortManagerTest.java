ad57a6c656bef20480dd7c5dae09a1163632ad1d
Yan Zhai <yanzhai@cs.wisc.edu>
2017-08-02 01:49:32 -0500

    unit test frame

--- src/test/java/org/latte/portlib/PortManagerTest.java
+++ src/test/java/org/latte/portlib/PortManagerTest.java
@@ -5,9 +5,9 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for PortManager
  */
-public class AppTest 
+public class PortManagerTest 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public PortManagerTest( String testName )
     {
         super( testName );
     }
@@ -25,13 +25,13 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( PortManagerTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testPortManager()
     {
         assertTrue( true );
     }
