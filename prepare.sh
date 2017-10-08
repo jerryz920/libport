81ddc44a53662d9d6034c331a3a39027bef16c19
Yan Zhai <yanzhai@cs.wisc.edu>
2017-10-08 17:59:47 -0500

    add necessary golang tool

--- prepare.sh
+++ prepare.sh
@@ -2,12 +2,14 @@
 apt-get install -y libboost-all-dev libssl-dev cmake3
 mkdir -p net
 cd net
-git clone https://github.com/Microsoft/cpprestsdk.git casablanca
+git clone -b dev https://github.com/jerryz920/cpprestsdk.git casablanca
 cd casablanca/Release
 mkdir -p build.release && cd build.release
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 make install
 
+go get github.com/sirupsen/logrus
+go get github.com/gorilla/mux
 
 
