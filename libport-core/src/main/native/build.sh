5503cc3cb8538248ad8f9db4d9785645f0c9c27d
Yan Zhai <zhaiyan920@gmail.com>
2018-03-05 18:23:26 -0600

    utility quick commit

--- libport-core/src/main/native/build.sh
+++ libport-core/src/main/native/build.sh
@@ -1,14 +1,15 @@
 
 
 for n in Debug Release; do
-if test -d build.$n; then
-cd build.$n
-else
-mkdir -p build.$n
-cd build.$n
-cmake .. -DCMAKE_BUILD_TYPE=$n
-fi
-make -j
-cd ..
+  if test -d build.$n; then
+    cd build.$n
+    cmake .. -DCMAKE_BUILD_TYPE=$n
+  else
+    mkdir -p build.$n
+    cd build.$n
+    cmake .. -DCMAKE_BUILD_TYPE=$n
+  fi
+  make -j
+  cd ..
 done
 
